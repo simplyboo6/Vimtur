40bc43252de663c70986dddbf2039d113f18a1db
SimplyBoo <simplyboo6@gmail.com>
2019-03-14 22:50:44 +0000

    Improved import/export and added scripts for docker

---
+++ export.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -z ${1} ]; then
+    echo "The first argument should be the filename to export to"
+    exit 1
+fi
+
+echo "Exporting to ${1}..."
+docker-compose exec vimtur /bin/sh -c 'rm -f /tmp/output.json && node /opt/app/src/utils/export_json.js -f /tmp/output.json'
+docker-compose exec vimtur /bin/sh -c 'cat /tmp/output.json' > ${1}
---
+++ import.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ -z ${1} ]; then
+    echo "The first argument should be the filename to import from"
+    exit 1
+fi
+
+echo "Importing ${1}..."
+cat ${1} | docker exec -i $(docker-compose ps -q vimtur) /bin/sh -c 'node /opt/app/src/utils/import_json.js --stdin'
