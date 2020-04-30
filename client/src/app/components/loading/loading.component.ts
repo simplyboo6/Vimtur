63b4fd3ab2d22731e8ee3602d78e506a116602d6
SimplyBoo <simplyboo6@gmail.com>
2020-04-30 08:15:54 +0100

    Added text for when searching done but theres no results

--- client/src/app/components/loading/loading.component.ts
+++ client/src/app/components/loading/loading.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Observable, of, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CollectionService } from 'app/services/collection.service';
 
@@ -8,13 +8,30 @@ import { CollectionService } from 'app/services/collection.service';
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss'],
 })
-export class LoadingComponent {
+export class LoadingComponent implements OnInit, OnDestroy {
   @Input() public title?: string;
+  public collection?: string[];
 
   private collectionService: CollectionService;
+  private subscriptions: Subscription[] = [];
 
   public constructor(collectionService: CollectionService) {
     this.collectionService = collectionService;
+  }
+
+  public ngOnInit() {
+    this.subscriptions.push(
+      this.collectionService.getMetadata().subscribe(metadata => {
+        this.collection = metadata.collection;
+      }),
+    );
+  }
+
+  public ngOnDestroy() {
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe();
+    }
+    this.subscriptions = [];
   }
 
   public getTitle(): Observable<string> {
