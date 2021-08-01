57cb7c2d4f2840d5f4bd98a0e449ec3d01554fbf
SimplyBoo <simplyboo6@gmail.com>
2021-08-01 20:10:02 +0100

    Fixed not rendering on desktop

--- client/src/app/components/viewer/viewer.component.ts
+++ client/src/app/components/viewer/viewer.component.ts
@@ -4,8 +4,6 @@ import { MediaService } from 'services/media.service';
 import { ConfigService } from 'services/config.service';
 import { Subscription } from 'rxjs';
 import { Media, Configuration } from '@vimtur/common';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { isMobile } from 'is-mobile';
 
 @Component({
@@ -17,7 +15,7 @@ export class ViewerComponent implements OnInit, OnDestroy {
   public tagsOpen = false;
   public media?: Media;
   public config?: Configuration.Main;
-  public maxWidthObservable: Observable<string>;
+  public mediaSrc?: string;
 
   private uiService: UiService;
   private mediaService: MediaService;
@@ -32,16 +30,6 @@ export class ViewerComponent implements OnInit, OnDestroy {
     this.uiService = uiService;
     this.mediaService = mediaService;
     this.configService = configService;
-
-    this.maxWidthObservable = this.configService.getConfiguration().pipe(
-      map(config => {
-        if (config.user.scaleToScreenWidthOnMobile && isMobile()) {
-          return `?maxWidth=${window.innerWidth}`;
-        } else {
-          return '';
-        }
-      }),
-    );
   }
 
   public ngOnInit() {
@@ -54,6 +42,14 @@ export class ViewerComponent implements OnInit, OnDestroy {
     this.subscriptions.push(
       this.mediaService.getMedia().subscribe(media => {
         this.media = media;
+        if (media) {
+          this.mediaSrc = `/api/images/${media.hash}/file`;
+          if (isMobile() && this.config?.user.scaleToScreenWidthOnMobile) {
+            this.mediaSrc = `${this.mediaSrc}?maxWidth=${window.innerWidth}`;
+          }
+        } else {
+          this.mediaSrc = undefined;
+        }
       }),
     );
 
