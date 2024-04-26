7e9da1cc6164eca0e74913eaa86b793044ea5b4c
SimplyBoo6 <simplyboo6@gmail.com>
2024-04-26 15:33:03 +0000

    Optimise SQLite queries and update server dependencies

--- server/src/database/sqlite/index.ts
+++ server/src/database/sqlite/index.ts
@@ -391,12 +391,16 @@ export class SqliteConnector extends Database {
   }
 
   public resetClones(): Promise<void> {
-    this.db.exec('UPDATE `media` SET `clones` = NULL');
+    this.db.exec(
+      'UPDATE `media` SET `clones` = NULL WHERE `hash` IN ((SELECT `hash` FROM `media` WHERE `clones` IS NOT NULL))',
+    );
     return Promise.resolve();
   }
 
   public async resetAutoTags(): Promise<void> {
-    this.db.exec('UPDATE `media` SET `auto_tags` = NULL');
+    this.db.exec(
+      'UPDATE `media` SET `auto_tags` = NULL WHERE `hash` IN ((SELECT `hash` FROM `media` WHERE `auto_tags` IS NOT NULL))',
+    );
     return Promise.resolve();
   }
 
